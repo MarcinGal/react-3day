77236ca1759ccccf8b43d1e9f92f2b05c347ea7a
marcingal <marcin921125@gmail.com>
2018-11-21 13:49:28 +0100

    change const to class

--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -1,14 +1,26 @@
 import React from 'react'
 
-import AppBar from 'material-ui/AppBar';
+import AppBar from 'material-ui/AppBar'
+import Drawer from 'material-ui/Drawer'
 
-const Navigation = (props) => (
-    <div>
-        <AppBar
-            title="Title"
-            iconClassNameRight="muidocs-icon-navigation-expand-more"
-        />
-    </div>
-)
+class Navigation extends React.Component {
+    render() {
+        return (
+            <div>
+                <AppBar
+                    title="Bar z plackami"
+                    onLeftIconButtonClick={() => alert('Clicked!')}
+                />
+                <Drawer
+                    docked={false}
+                    width={200}
+                    open={true}
+                    onRequestChange={(open) => console.log(open)}
+                >
+                </Drawer>
+            </div >
+        )
+    }
+}
 
 export default Navigation
